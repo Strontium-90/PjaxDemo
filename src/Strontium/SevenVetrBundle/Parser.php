4da29fe8e7728779a13fa4f4f3b4061b41c125a2
Aleksey Bannov <a.s.bannov@gmail.com>
2017-04-23 18:14:22 +0300

    7vetr test

--- src/Strontium/SevenVetrBundle/Parser.php
+++ src/Strontium/SevenVetrBundle/Parser.php
@@ -9,6 +9,15 @@ class Parser
 {
     public function parse($text)
     {
-        return [];
+        $description = [];
+        $data = [];
+        preg_match_all('|\[([^\]+])(:([^\]]+))?\](.*)\[\/\1\]|Uis', $text, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $i => $match) {
+            $data[$match[1]] = $match[4];
+            $description[$match[1]] = $match[3];
+        }
+
+        return [$data, $description];
     }
 }
--- src/Strontium/SevenVetrBundle/spec/ParserSpec.php
+++ src/Strontium/SevenVetrBundle/spec/ParserSpec.php
@@ -15,13 +15,47 @@ class ParserSpec extends ObjectBehavior
         $this->shouldHaveType('Strontium\SevenVetrBundle\Parser');
     }
 
-    private function it_should_parse_this_text()
+    function it_should_parse_this_text()
     {
         $text = <<<TEXT
-
+The home directory [a:aaa]inside[/a] which Python MySQL settings will be stored, which Ansible will use when connecting to MySQL.
+This should be the home directory of the user which runs this [e]Ansible[/d] role. The mysql_user_name and mysql_user_password 
+can be set if you are [b:bbb]running[/b] this role [c]under[/c] a non-root user account and want to set a non-root user.
 TEXT;
 
 
-        $this->parse($text)->shoulBe([]);
+        $this->parse($text)->shouldBeEqualTo([
+            [
+                'a' => 'inside',
+                'b' => 'running',
+                'c' => 'under',
+            ],
+            [
+                'a' => 'aaa',
+                'b' => 'bbb',
+                'c' => '',
+            ]
+        ]);
+    }
+
+    function it_should_parse_only_unique_tags()
+    {
+        $text = <<<TEXT
+The home directory [a:aaa]inside[/a] which Python MySQL settings will be stored, which Ansible will use when connecting to MySQL.
+This should be the home directory of the user which runs this Ansible role. The mysql_user_name and mysql_user_password 
+can be set if you are [b:bbb]running[/b] this role [a]under[/a] a non-root user account and want to set a non-root user.
+TEXT;
+
+
+        $this->parse($text)->shouldBeEqualTo([
+            [
+                'a' => 'under',
+                'b' => 'running',
+            ],
+            [
+                'a' => '',
+                'b' => 'bbb',
+            ]
+        ]);
     }
 }
